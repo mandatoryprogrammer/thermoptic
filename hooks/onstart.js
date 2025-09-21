77488852eb1eed8b42d67b6adc05ae18c0ddf297
mandatory <mandatory@gmail.com>
2025-09-21 17:57:18 -0400

    Added new hook example that clicks Cloudflare's turnstile CAPTCHA

--- hooks/onstart.js
+++ hooks/onstart.js
@@ -18,6 +18,22 @@ function get_cdp_config() {
     }
 }
 
+// This text means we have to click the CAPTCHA to continue
+const captcha_solve_required_text = `Verify you are human by completing the action below.`;
+
+// This text means a JavaScript check if running on the turnstile.
+const javascript_check_text = `Verifying you are human. This may take a few seconds`;
+
+function rand_int(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // This function is called upon starting thermoptic
 // You can visit pages, perform actions that require
 // a full browser, etc here.
@@ -35,9 +51,9 @@ export async function hook(cdp) {
     const { Target } = cdp;
     const { targetId } = await Target.createTarget({ url: 'about:blank' });
     let init_params = get_cdp_config();
-    init_params.target = targetId; 
+    init_params.target = targetId;
     const client = await CDP(init_params);
-    const { Page, DOM } = client;
+    const { Page, DOM, Input } = client;
 
     await Page.enable();
     await DOM.enable();
@@ -60,7 +76,84 @@ export async function hook(cdp) {
             continue;
         }
 
-        if (!outerHTML.includes('Verifying you are human. This may take a few seconds')) {
+        if (outerHTML.includes(captcha_solve_required_text)) {
+            console.log(`[STATUS] A Cloudflare turnstile CAPTCHA has been thrown, attempting to click through it...`);
+
+            // Wait some fuzzy amount of time before clicking so we're
+            // not superhuman with it :) 
+            const fuzzy_wait_ms = rand_int((1000 * 1.5), (1000 * 5));
+            console.log(`[STATUS] Waiting ${fuzzy_wait_ms} milliseconds so we don't appear super human...`);
+            await sleep(fuzzy_wait_ms);
+
+            const { root: { nodeId: documentNodeId } } = await DOM.getDocument({ depth: -1, pierce: true });
+
+            // Get all div elements in the document
+            const { nodeIds: divNodeIds } = await DOM.querySelectorAll({
+                nodeId: documentNodeId,
+                selector: 'div'
+            });
+
+            let targetNodeId = null;
+
+            for (const nodeId of divNodeIds) {
+                const { attributes } = await DOM.getAttributes({ nodeId });
+
+                // Convert array of [name1, value1, name2, value2, ...] to an object
+                const attrs = {};
+                for (let i = 0; i < attributes.length; i += 2) {
+                    attrs[attributes[i]] = attributes[i + 1];
+                }
+
+                if (attrs.style && attrs.style.includes('display: grid')) {
+                    targetNodeId = nodeId;
+                    break;
+                }
+            }
+
+            const { model } = await DOM.getBoxModel({ nodeId: targetNodeId });
+
+            // model.content = [x1, y1, x2, y2, x3, y3, x4, y4]
+            const x_top_left = model.content[0];
+            const y_top_left = model.content[1];
+            const x_bottom_left = model.content[6]; // x3
+            const y_bottom_left = model.content[7]; // y3
+
+            // Fuzz our click coordinates a bit so we're not weirdly
+            // clicking in the precise place each time
+            const x_fuzz = rand_int(-5, 5);
+            const y_fuzz = rand_int(-5, 5);
+
+            // Width is not strictly needed; we want 25px from left
+            const click_x = (x_top_left + 25) + x_fuzz;
+
+            // Vertical center: average of top and bottom y-coordinates
+            const click_y = ((y_top_left + y_bottom_left) / 2) + y_fuzz;
+
+            console.log('[STATUS] Clicking CAPTCHA checkbox at coordinates: ', click_x, click_y);
+
+            // Then dispatch the mouse event
+            await Input.dispatchMouseEvent({
+                type: 'mousePressed',
+                x: click_x,
+                y: click_y,
+                button: 'left',
+                clickCount: 1
+            });
+
+            await Input.dispatchMouseEvent({
+                type: 'mouseReleased',
+                x: click_x,
+                y: click_y,
+                button: 'left',
+                clickCount: 1
+            });
+
+            console.log(`[STATUS] Clicked checkbox, waiting a bit for that to work...`);
+            // Now we wait for the checking action to do something before we rescan
+            await sleep((1000 * 2));
+        }
+
+        if (!outerHTML.includes(javascript_check_text) && !outerHTML.includes(captcha_solve_required_text)) {
             console.log(`[STATUS] Passed Cloudflare's JavaScript check! Moving along...`);
             break;
         }
