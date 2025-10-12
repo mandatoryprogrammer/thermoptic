295e7cc78146a8afc18a0a23687e581f8b3b146f
mandatory <mandatory@gmail.com>
2025-10-12 17:33:12 -0400

    Restart chrome even with a non-zero exit code

--- chrome/start.sh
+++ chrome/start.sh
@@ -17,13 +17,21 @@ socat TCP-LISTEN:3003,fork TCP:127.0.0.1:3002 &
 
 # Launch chrome with the debugging port
 while true; do
+  set +e
   /usr/bin/google-chrome-stable \
     --remote-debugging-port=3002 \
     --remote-debugging-address=0.0.0.0 \
     --no-sandbox \
     --user-data-dir=/tmp/chrome-profile \
     "about:blank"
+  chrome_status=$?
+  set -e
 
-  echo "Chrome crashed or exited. Restarting in 2 seconds..."
-  sleep 2
+  if [ "${chrome_status}" -eq 0 ]; then
+    echo "[STATUS] Chrome exited cleanly. Restarting in 1 second..."
+  else
+    echo "[WARN] Chrome exited with code ${chrome_status}. Restarting in 2 seconds..."
+  fi
+
+  sleep 1
 done
